ae26f90a69d7d127a8bcd78fa68c384945a0d471
Duru <derindagsd@gmail.com>
2023-03-22 16:06:23 +0300

    Completed Problem Session 3

--- PS03/q1.cpp
+++ PS03/q1.cpp
@@ -2,53 +2,69 @@
 
 using namespace std;
 
-class Coordinates
+class HotDogStand
 {
 private:
-    double x;
-    double y;
+    int idNo;
+    int soldToday;
+    static int totalHotDogsSold;
 
 public:
-    // Setter functions (mutators)
-    void set_coordinates(double a, double b)
+    // Constructors
+    HotDogStand() {} // Default Constructor
+
+    HotDogStand(int id, int sold) // Constructor which initializes stand's ID number and # of hotdogs sold today
     {
-        x = a;
-        y = b;
+        idNo = id;
+        soldToday = sold;
+
+        totalHotDogsSold += sold;
     }
 
-    void set_x_coordinate(double a) { x = a; }
-    void set_y_coordinate(double b) { y = b; }
-
-    // Function to move the points to a specified distance
-    void move_distance(double a, double b)
+    // Member Functions
+    void JustSold()
     {
-        /* QUESTION!!! should the coordinates move from a certain
-        point or set a new point */
-        x += a;
-        y += b;
+        // Increments the stand'd sold number and the total sold amount
+        cout << "stand #" << idNo << " just made a sale!\n"
+             << endl;
+        soldToday++;
+        totalHotDogsSold++;
     }
 
-    // Function to rotate 90 degrees
-    void rotate_90_degrees()
-    {
-        double tempX = x, tempY = y;
-
-        x = tempY;
-        y = tempX * (-1.0);
-    }
-
-    double get_x_coordinate() const { return x; }
-    double get_y_coordinate() const { return y; }
+    // Getter functions
+    int get_hotdogs_sold() { return soldToday; }
+    int get_total_hotdogs_sold() { return totalHotDogsSold; }
+    int get_id() { return idNo; }
 };
+
+// Initializing static integer
+int HotDogStand::totalHotDogsSold = 0;
 
 int main()
 {
-    Coordinates c1;
+    HotDogStand stand1(1, 0);
+    HotDogStand stand2(2, 100);
+    HotDogStand stand3(3, 23);
 
-    c1.set_coordinates(3, 7);
-    cout << c1.get_x_coordinate() << "," << c1.get_y_coordinate() << endl;
-    c1.move_distance(0, 3);
-    cout << c1.get_x_coordinate() << "," << c1.get_y_coordinate() << endl;
-    c1.rotate_90_degrees();
-    cout << c1.get_x_coordinate() << "," << c1.get_y_coordinate() << endl;
+    cout << "Stand 1 sold today: " << stand1.get_hotdogs_sold() << endl;
+    cout << "Stand 2 sold today: " << stand2.get_hotdogs_sold() << endl;
+    cout << "Stand 3 sold today: " << stand3.get_hotdogs_sold() << endl;
+    cout << "Total number of hotdogs sold ever by everyone: " << stand1.get_total_hotdogs_sold() << endl;
+
+    stand1.JustSold();
+
+    cout << "Stand 1 sold today: " << stand1.get_hotdogs_sold() << endl;
+
+    stand2.JustSold();
+    stand2.JustSold();
+    stand3.JustSold();
+    stand1.JustSold();
+    stand3.JustSold();
+    stand3.JustSold();
+
+    cout << "==============\nThe hotdog stands are now closed." << endl;
+    cout << "Stand 1 sold today: " << stand1.get_hotdogs_sold() << endl;
+    cout << "Stand 2 sold today: " << stand2.get_hotdogs_sold() << endl;
+    cout << "Stand 3 sold today: " << stand3.get_hotdogs_sold() << endl;
+    cout << "Total number of hotdogs sold ever by everyone: " << stand1.get_total_hotdogs_sold() << endl;
 }
