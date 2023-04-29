ccef5eef9ad81191779a4ff03f657b5b98ef1263
Duru <derindagsd@gmail.com>
2023-04-29 12:38:01 +0300

    updated Course member functions

--- School Mngmnt. System/pa4/Course.cpp
+++ School Mngmnt. System/pa4/Course.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include "Course.h"
+#include "Student.h"
+
+const std::string SENT_VAL = "-999";
+const int INIT_CAPACITY = 10;
 
 namespace
 {
@@ -9,4 +13,67 @@ namespace
 namespace PA4
 {
     void Course::function1() { std::cout << "Hello Course" << std::endl; }
+
+    Course::Course()
+    {
+        studentsEnrolledSize = 0;
+        studentsEnrolledCapacity = INIT_CAPACITY;
+        studentsEnrolled = new Student[studentsEnrolledCapacity];
+    }
+
+    Course::Course(const Course &copy)
+        : name(copy.name), code(copy.code), studentsEnrolledSize(copy.studentsEnrolledSize), studentsEnrolledCapacity(copy.studentsEnrolledCapacity)
+    {
+        // Create new array for the copied students
+        studentsEnrolled = new Student[studentsEnrolledCapacity];
+
+        // Copy the students from the original course
+        for (int i = 0; i < studentsEnrolledSize; i++)
+            studentsEnrolled[i] = copy.studentsEnrolled[i];
+    }
+
+    Course::Course(std::string n, std::string c)
+        : name(n), code(c)
+    {
+        studentsEnrolledSize = 0;
+        studentsEnrolledCapacity = INIT_CAPACITY;
+        studentsEnrolled = new Student[studentsEnrolledCapacity];
+    }
+
+    Course::~Course()
+    {
+        if (studentsEnrolled != NULL)
+            delete[] studentsEnrolled;
+    }
+
+    void Course::set_name_code(std::string n, std::string c)
+    {
+        name = n;
+        code = c;
+    }
+
+    std::string Course::get_name() const { return name; }
+    std::string Course::get_code() const { return code; }
+    Student *Course::get_students() const { return studentsEnrolled; }
+
+    Course &Course::operator=(const Course &other)
+    { // Check for self-assignment
+        if (this != &other)
+        {
+            // Copy the name and ID
+            name = other.name;
+            code = other.code;
+
+            // Copy the courses
+            delete[] studentsEnrolled;
+            studentsEnrolledSize = other.studentsEnrolledSize;
+            studentsEnrolledCapacity = other.studentsEnrolledCapacity;
+            studentsEnrolled = new Student[studentsEnrolledCapacity];
+            for (int i = 0; i < studentsEnrolledSize; i++)
+            {
+                studentsEnrolled[i] = other.studentsEnrolled[i];
+            }
+        }
+        return *this;
+    }
 }
--- School Mngmnt. System/pa4/Course.h
+++ School Mngmnt. System/pa4/Course.h
@@ -1,12 +1,44 @@
 #ifndef COURSE_H_
 #define COURSE_H_
 
+#include "Student.h"
+
 namespace PA4
 {
+    class Student;
     class Course
     {
     public:
         void function1();
+
+        // Constructors & Destructor
+        Course();
+        Course(const Course &copy);
+        Course(std::string n, std::string c);
+        // Course(std::string n, int c, Student *students);
+
+        Course(const Student &students);
+        ~Course();
+
+        // Setter Functions
+        void set_name_code(std::string n, std::string c);
+        // void set_students(const Course &courses);
+
+        // Getter Functions
+        std::string get_name() const;
+        std::string get_code() const;
+        Student *get_students() const;
+
+        Course &operator=(const Course &other);
+
+    private:
+        std::string name;
+        std::string code;
+
+        // studentsEnroller pointer points to a dynamic array of students
+        Student *studentsEnrolled;
+        int studentsEnrolledSize;
+        int studentsEnrolledCapacity;
     };
 }
 
